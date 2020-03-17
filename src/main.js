4a6c6beb05e92e98d4c8e25e7db48d8bc81cab63
yandong <243530612@qq.com>
2020-03-17 15:00:21 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {
+  Button
+} from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
